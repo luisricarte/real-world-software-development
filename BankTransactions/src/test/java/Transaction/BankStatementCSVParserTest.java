4ee69bbc0ec709d3ea7ebbc70a5a463ff893d2a4
Luis Ricarte <luis.ricarte@ccc.ufcg.edu.br>
2024-12-27 14:27:46 -0300

    page 28: add new test - junity

--- BankTransactions/src/test/java/Transaction/BankStatementCSVParserTest.java
+++ BankTransactions/src/test/java/Transaction/BankStatementCSVParserTest.java
@@ -28,4 +28,17 @@ public class BankStatementCSVParserTest {
         Assert.assertEquals(expected.getAmount(), result.getAmount(),tolerance);
         Assert.assertEquals(expected.getDescription(), result.getDescription());
     }
+
+    @Test
+    public void shouldParseOneCorrectLineDebit() throws Exception {
+        final String line = "31-12-2020,310,Debit";
+
+        final BankTransaction result = statementCSVParser.parseFromCSV(line);
+        final BankTransaction expected = new BankTransaction(LocalDate.of(2020, Month.DECEMBER, 31), 310, "Debit");
+        final double tolerance = 0.0d;
+
+        Assert.assertEquals(expected.getDate(), result.getDate());
+        Assert.assertEquals(expected.getAmount(), result.getAmount(),tolerance);
+        Assert.assertEquals(expected.getDescription(), result.getDescription());
+    }
 }
